65bb05a782a18dadb563dfa29b0d83d8efd6cec5
unknown <hanadi.hosri@gmail.com>
2023-10-28 21:45:28 +0300

    displayMenu func

--- Assignment3.py
+++ Assignment3.py
@@ -1,8 +1,10 @@
 name = input("enter your name :")
 print("Welcome!!", name)
-print("1. Add Matrices\n" + "2. Check Rotation\n" + "3. Invert Dictionary\n" +
+def displayMenu():
+   print("1. Add Matrices\n" + "2. Check Rotation\n" + "3. Invert Dictionary\n" +
         "4. Convert Matrix to Dictionary\n" +"5. Check Palindrome\n" +
          "6. Search for an Element & Merge Sort\n" + "7. Exit")
+displayMenu()
 
 def addMatrices(rows,cols): #O(N^2) , N being the number of rows or cols
     matrix1 = []
