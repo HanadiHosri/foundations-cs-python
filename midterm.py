d825e522e1cc6d41a12de36fac72c00ef7cab084
unknown <hanadi.hosri@gmail.com>
2023-11-10 15:13:09 +0200

    removing midterm

--- midterm.py
+++
@@ -1,8 +0,0 @@
-def displayMenu():
-   print("1. Open Tab\n" + "2. Close Tab\n" + "3. Switch Tab\n" + "4. Display All Tabs\n" +"5. Open Nested Tab\n" + "6. Clear All Tabs\n"+ "7. Save Tabs\n" + "8. Import Tabs\n" + "9. Exit")
-
-def openTab():
-   title = input("enter title of the website :")
-   url = input("enter URL of the website :")
-   tab = {}
-   tab[url]=[title]
