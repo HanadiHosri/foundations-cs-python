b250b9f927e1fbe907b94ff2a8fe5897fee2df78
unknown <hanadi.hosri@gmail.com>
2023-11-11 18:45:16 +0200

    adding choice3

--- midterm/midterm.py
+++ midterm/midterm.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 def displayMenu():
    print("1. Open Tab\n" + "2. Close Tab\n" + "3. Switch Tab\n" + "4. Display All Tabs\n" +"5. Open Nested Tab\n" + "6. Clear All Tabs\n"+ "7. Save Tabs\n" + "8. Import Tabs\n" + "9. Exit")
@@ -12,27 +13,34 @@ def closeTab(lst,index):
    lst.remove(lst[index])
 
 def switchTab(url):
-   response = requests.get(url)
-   return response.text
+   r = requests.get(url)
+   soup = BeautifulSoup(r.content, 'html.parser') 
+   return soup.prettify()
+   
 
 def main():
    print("Greetings !")
    choice = 0
    open_tabs = []
    while choice != 9:
-    displayMenu()
-    choice = int(input("enter your choice :"))
-    if choice == 1:
-        title = input("enter title of the website :")
-        url = input("enter URL of the website :")
-        open_tabs.append(openTab(title,url))
-    elif choice == 2:
-        i = input("enter the index of the tab you wish to close :")
-        if i == "":
-           closeTab(open_tabs,len(open_tabs)-1)
-        else:
-           i = int(i)
-           closeTab(open_tabs,i) 
-      
+      displayMenu()
+      choice = int(input("enter your choice :"))
+      if choice == 1:
+         title = input("enter title of the website :")
+         url = input("enter URL of the website :")
+         open_tabs.append(openTab(title,url))
+      elif choice == 2:
+         i = input("enter the index of the tab you wish to close :")
+         if i == "":
+            closeTab(open_tabs,len(open_tabs)-1)
+         else:
+            i = int(i)
+            closeTab(open_tabs,i) 
+      elif choice == 3:
+         i = input("enter the index of the tab to display its content :")
+         tab = open_tabs[i]
+         for url in tab:
+            u = url
+         print(switchTab(u))
 main()
 
