897bcf334f6d4103283c39e8f16d731623be4a6b
unknown <hanadi.hosri@gmail.com>
2023-11-19 16:37:02 +0200

    adding menu1

--- Assignment4.py
+++ Assignment4.py
@@ -1,3 +1,6 @@
 def displayMenu():
     print("1. Singly Linked List\n" + "2. Check if Palindrome\n" + "3. Priority Queue\n" +
         "4. Evaluate an Infix Expression\n" +"5. Graph\n" + "6. Exit")
+    
+def displayMenu1():
+    print("a. Add Node\n" + "b. Display Nodes\n" + "c. Search for & Delete Node\n" + "d. Return to main menu")
