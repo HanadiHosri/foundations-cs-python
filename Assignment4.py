deef3ac476051ccb62593bc41fbe3da7a25b421b
unknown <hanadi.hosri@gmail.com>
2023-11-19 16:46:00 +0200

    adding main

--- Assignment4.py
+++ Assignment4.py
@@ -4,3 +4,19 @@ def displayMenu():
     
 def displayMenu1():
     print("a. Add Node\n" + "b. Display Nodes\n" + "c. Search for & Delete Node\n" + "d. Return to main menu")
+
+
+
+
+def main():
+  name = input("enter your name :")
+  print("Hello, Welcome!!", name)
+  choice = 0
+  while choice != 7:
+    displayMenu()
+    choice = int(input("enter your choice :"))
+    if choice == 1:
+       displayMenu1()
+       choice1 = input("enter your choice :")
+       if choice1 == "a":
+          
